8ebfc0f3a82807730ac8ee369e4dfd11cae8945f
Sciss <contact@sciss.de>
2010-07-18 19:09:11 +0100

    ...

---
+++ src/main/scala/de/sciss/nuages/NuagesConfig.scala
@@ -0,0 +1,11 @@
+package de.sciss.nuages
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rutz
+ * Date: 18.07.2010
+ * Time: 15:36:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+object NuagesConfig
