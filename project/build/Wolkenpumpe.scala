522f375c8a6a62a6095dd5e5e15510cbaf8527d1
Sciss <contact@sciss.de>
2010-08-11 15:21:01 +0100

    ...

--- project/build/Wolkenpumpe.scala
+++ project/build/Wolkenpumpe.scala
@@ -7,20 +7,10 @@ import sbt.{ FileUtilities => FU, _}
 class WolkenpumpeProject( info: ProjectInfo ) extends DefaultProject( info ) {
    // stupidly, we need to redefine the dependancy here, because
    // for some reason, sbt will otherwise try to look in the maven repo
-   val dep1 = "de.sciss" %% "scalaaudiofile" % "0.13"
-   val dep2 = "de.sciss" %% "scalaosc" % "0.19"
-   val dep3 = "de.sciss" %% "scalacollider" % "0.17"
-   val dep4 = "prefuse" % "prefuse" % "beta-20071021" from "http://github.com/downloads/Sciss/ScalaColliderSwing/prefuse-beta-20071021.jar"
-   val dep5 = "de.sciss" %% "scalacolliderswing" % "0.17"
-   val dep6 = "de.sciss" %% "soundprocesses" % "0.14"
-   val dep9 = "Stanford CS - Pervasive Parallelism Laboratory" %% "ccstm" % "0.2"
-
-   // ---- ccstm dependancies ----
-   val scalatest = crossScalaVersionString match {
-     // RC7 is the same as the release, but scalatest against the release is not
-     // yet available
-     case "2.8.0" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT"
-     case _ => "org.scalatest" % "scalatest" % ("1.2-for-scala-" + crossScalaVersionString + "-SNAPSHOT")
-   }
-   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+   val scalaAudioFile      = "de.sciss" %% "scalaaudiofile" % "0.13"
+   val scalaOSC            = "de.sciss" %% "scalaosc" % "0.19"
+   val scalaCollider       = "de.sciss" %% "scalacollider" % "0.17"
+   val prefuse             = "prefuse" % "prefuse" % "beta-20071021" from "http://github.com/downloads/Sciss/ScalaColliderSwing/prefuse-beta-20071021.jar"
+   val scalaColliderSwing  = "de.sciss" %% "scalacolliderswing" % "0.17"
+   val soundProcesses      = "de.sciss" %% "soundprocesses" % "0.15"
 }
